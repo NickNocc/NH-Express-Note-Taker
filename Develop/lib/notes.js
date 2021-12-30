8ccbf5faf94df33d542ecbf03010d4f02146f694
NickNocc <haretakis4@gmail.com>
2021-12-30 11:16:21 -0800

    Keep it simple stupid

---
+++ Develop/lib/notes.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+function createNewNote(body, noteArray) {
+    const newNote = body;
+    noteArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ noteArray }, null, 2)
+    )
+}
+module.exports = { createNewNote };
--- Develop/server.js
+++ Develop/server.js
@@ -2,15 +2,32 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// Get api notes
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+// Post notes to database
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    console.log(req.body);
+    // Need a function to turn the info into a an object
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'))
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
